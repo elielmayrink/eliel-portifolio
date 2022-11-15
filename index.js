bbc152a6cf7096f96dc5cd48a23812f34df990ef
Eliel Mayrimk <elielmayrink2@gmail.com>
2022-11-15 08:29:55 -0300

    html css e js iniciaisdo projeto

---
+++ index.js
@@ -0,0 +1,46 @@
+"use strict";
+let projects = document.querySelector("#projects");
+async function getGithubRepos() {
+  const username = "elielmayrink";
+  const res = await fetch(`http://api.github.com/users/${username}/repos`);
+  const response = await res.json();
+  for (let i = 0; i < response.length; i++) {
+    const project = document.createElement("div");
+    project.className = "project";
+    project.innerHTML = `<div class="project-name">
+    <img src="./assets/imagens/icons/folder.png" alt="" />
+    <h5>${response[i].full_name}</h5>
+  </div>
+  <div class="project-description">
+<p>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque
+consectetur iure, reprehenderit molestiae fugiat molestias
+</p>
+  </div>
+  <div class="project-complement">
+<div>
+<span
+  ><img src="./assets/imagens/icons/star.png" alt="" />100</span
+>
+<span
+  ><img
+    src="./assets/imagens/icons/git-branch.png"
+    alt=""
+  />100</span
+>
+</div>
+<div>
+<span
+  ><img
+    src="./assets/imagens/icons/Ellipse.png"
+    alt=""
+  />javascript</span
+>
+</div>
+  </div>`;
+    projects.appendChild(project);
+  }
+  console.log(response);
+}
+projects.addEventListener("load", getGithubRepos());
+// getGithubRepos();
