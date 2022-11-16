7bdd44091eda32202feb0d54c5f77f5c8cf675d8
Eliel Mayrimk <elielmayrink2@gmail.com>
2022-11-16 05:49:38 -0300

    adicionando informações dinamicas dos repos

--- index.js
+++ index.js
@@ -2,31 +2,46 @@
 let projects = document.querySelector("#projects");
 async function getGithubRepos() {
   const username = "elielmayrink";
-  const res = await fetch(`https://api.github.com/users/${username}/repos`);
+  const res = await fetch(
+    `https://api.github.com/users/${username}/repos?per_page=12`
+  );
   const response = await res.json();
-  for (let i = 0; i < response.length; i++) {
+  const repos = response.map((repo) => ({
+    name: repo.name,
+    id: repo.id,
+    html_url: repo.html_url,
+    stargazers_count: repo.stargazers_count,
+    language: repo.language,
+    description: repo.description,
+  }));
+  for (let i = 0; i < repos.length; i++) {
     const project = document.createElement("div");
     project.className = "project";
     project.innerHTML = `<div class="project-name">
     <img src="./assets/imagens/icons/folder.png" alt="" />
-    <h5>${response[i].full_name}</h5>
+    <h5>${repos[i].name}</h5>
   </div>
   <div class="project-description">
 <p>
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque
-consectetur iure, reprehenderit molestiae fugiat molestias
+${
+  repos[i].description
+    ? repos[i].description
+    : "Repositório sem descrição adicional"
+}
 </p>
   </div>
   <div class="project-complement">
 <div>
 <span
-  ><img src="./assets/imagens/icons/star.png" alt="" />100</span
+  ><img src="./assets/imagens/icons/star.png" alt="" />${
+    repos[i].stargazers_count
+  }</span
 >
 <span
   ><img
     src="./assets/imagens/icons/git-branch.png"
     alt=""
-  />100</span
+  />1</span
 >
 </div>
 <div>
@@ -34,7 +49,7 @@ consectetur iure, reprehenderit molestiae fugiat molestias
   ><img
     src="./assets/imagens/icons/Ellipse.png"
     alt=""
-  />javascript</span
+  />${repos[i].language ? repos[i].language : "HTML"}</span
 >
 </div>
   </div>`;
@@ -42,5 +57,3 @@ consectetur iure, reprehenderit molestiae fugiat molestias
   }
 }
 projects.addEventListener("load", getGithubRepos());
-
-// getGithubRepos();
